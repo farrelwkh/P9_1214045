91dbdfc9774f25d107911594f2a897052c0665c4
farrelwkh <110893535+farrelwkh@users.noreply.github.com>
2022-12-12 16:31:59 +0700

    Add files via upload

---
+++ pelajaran9_1214045/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace pelajaran9_1214045
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Npm = new System.Windows.Forms.Label();
+            this.Nama = new System.Windows.Forms.Label();
+            this.Angkatan = new System.Windows.Forms.Label();
+            this.Alamat = new System.Windows.Forms.Label();
+            this.Email = new System.Windows.Forms.Label();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.BtnSimpan = new System.Windows.Forms.Button();
+            this.BtnUbah = new System.Windows.Forms.Button();
+            this.BtnHapus = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Nohp = new System.Windows.Forms.Label();
+            this.DataMahasiswa = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataMahasiswa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DataMahasiswa);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1571, 398);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tabel Data Mahasiswa";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Nohp);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.Email);
+            this.groupBox2.Controls.Add(this.Alamat);
+            this.groupBox2.Controls.Add(this.Angkatan);
+            this.groupBox2.Controls.Add(this.Nama);
+            this.groupBox2.Controls.Add(this.Npm);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 404);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1069, 304);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Form Data Mahasiswa";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.BtnHapus);
+            this.groupBox3.Controls.Add(this.BtnUbah);
+            this.groupBox3.Controls.Add(this.BtnSimpan);
+            this.groupBox3.Controls.Add(this.BtnRefresh);
+            this.groupBox3.Location = new System.Drawing.Point(1087, 440);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(484, 268);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tombol Action";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(140, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(470, 28);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Npm
+            // 
+            this.Npm.AutoSize = true;
+            this.Npm.Location = new System.Drawing.Point(6, 50);
+            this.Npm.Name = "Npm";
+            this.Npm.Size = new System.Drawing.Size(42, 20);
+            this.Npm.TabIndex = 6;
+            this.Npm.Text = "Npm";
+            // 
+            // Nama
+            // 
+            this.Nama.AutoSize = true;
+            this.Nama.Location = new System.Drawing.Point(6, 83);
+            this.Nama.Name = "Nama";
+            this.Nama.Size = new System.Drawing.Size(51, 20);
+            this.Nama.TabIndex = 7;
+            this.Nama.Text = "Nama";
+            // 
+            // Angkatan
+            // 
+            this.Angkatan.AutoSize = true;
+            this.Angkatan.Location = new System.Drawing.Point(6, 120);
+            this.Angkatan.Name = "Angkatan";
+            this.Angkatan.Size = new System.Drawing.Size(78, 20);
+            this.Angkatan.TabIndex = 8;
+            this.Angkatan.Text = "Angkatan";
+            // 
+            // Alamat
+            // 
+            this.Alamat.AutoSize = true;
+            this.Alamat.Location = new System.Drawing.Point(6, 163);
+            this.Alamat.Name = "Alamat";
+            this.Alamat.Size = new System.Drawing.Size(63, 20);
+            this.Alamat.TabIndex = 9;
+            this.Alamat.Text = "Alamat ";
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(6, 225);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(48, 20);
+            this.Email.TabIndex = 10;
+            this.Email.Text = "Email";
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Location = new System.Drawing.Point(66, 66);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(334, 27);
+            this.BtnRefresh.TabIndex = 0;
+            this.BtnRefresh.Text = "Refresh";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // BtnSimpan
+            // 
+            this.BtnSimpan.Location = new System.Drawing.Point(66, 121);
+            this.BtnSimpan.Name = "BtnSimpan";
+            this.BtnSimpan.Size = new System.Drawing.Size(334, 26);
+            this.BtnSimpan.TabIndex = 1;
+            this.BtnSimpan.Text = "Simpan";
+            this.BtnSimpan.UseVisualStyleBackColor = true;
+            // 
+            // BtnUbah
+            // 
+            this.BtnUbah.Location = new System.Drawing.Point(66, 173);
+            this.BtnUbah.Name = "BtnUbah";
+            this.BtnUbah.Size = new System.Drawing.Size(334, 26);
+            this.BtnUbah.TabIndex = 2;
+            this.BtnUbah.Text = "Ubah";
+            this.BtnUbah.UseVisualStyleBackColor = true;
+            // 
+            // BtnHapus
+            // 
+            this.BtnHapus.Location = new System.Drawing.Point(66, 219);
+            this.BtnHapus.Name = "BtnHapus";
+            this.BtnHapus.Size = new System.Drawing.Size(334, 29);
+            this.BtnHapus.TabIndex = 3;
+            this.BtnHapus.Text = "Hapus";
+            this.BtnHapus.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(470, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(140, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(470, 26);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(140, 258);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(470, 26);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(140, 157);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(470, 62);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(140, 226);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(470, 26);
+            this.textBox5.TabIndex = 13;
+            // 
+            // Nohp
+            // 
+            this.Nohp.AutoSize = true;
+            this.Nohp.Location = new System.Drawing.Point(6, 258);
+            this.Nohp.Name = "Nohp";
+            this.Nohp.Size = new System.Drawing.Size(47, 20);
+            this.Nohp.TabIndex = 14;
+            this.Nohp.Text = "Nohp";
+            // 
+            // DataMahasiswa
+            // 
+            this.DataMahasiswa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataMahasiswa.Location = new System.Drawing.Point(3, 22);
+            this.DataMahasiswa.Name = "DataMahasiswa";
+            this.DataMahasiswa.RowHeadersWidth = 62;
+            this.DataMahasiswa.RowTemplate.Height = 28;
+            this.DataMahasiswa.Size = new System.Drawing.Size(1562, 376);
+            this.DataMahasiswa.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1583, 720);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DataMahasiswa)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label Nohp;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label Alamat;
+        private System.Windows.Forms.Label Angkatan;
+        private System.Windows.Forms.Label Nama;
+        private System.Windows.Forms.Label Npm;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button BtnHapus;
+        private System.Windows.Forms.Button BtnUbah;
+        private System.Windows.Forms.Button BtnSimpan;
+        private System.Windows.Forms.Button BtnRefresh;
+        private System.Windows.Forms.DataGridView DataMahasiswa;
+    }
+}
+
---
+++ pelajaran9_1214045/Koneksi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pelajaran9_1214045
+{
+    internal class Koneksi
+    {
+        string conectionstring = "Server=localhost;Database=ulbi;vid=root;Pwd=;";
+        MySqlConnecti kon;
+
+        public void OpenConnection()
+        {
+
+        }
+    }
+}
